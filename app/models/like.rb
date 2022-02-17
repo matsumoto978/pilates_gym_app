d9481249a0d3d3bec964d62f67a1300cfab9d909
matsumoto978 <myoshi878@gmail.com>
2022-02-18 03:30:48 +0900

    多対多の関連づけ

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  
+  validate :my_post_cannot_like
+
+  private
+
+  def my_post_cannot_like
+    errors.add(:base, "自分の投稿にいいねはできません") if user_id == post.user_id
+  end
 end
