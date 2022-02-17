fc6749dfbc742e47598da77570d257fca9642763
matsumoto978 <myoshi878@gmail.com>
2022-02-18 02:01:40 +0900

    ゲストユーザーの名前の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
     end
   end
 end
