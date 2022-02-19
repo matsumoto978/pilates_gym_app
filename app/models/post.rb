be3061fadd20e4bd21a7a6253531f4205352415a
matsumoto978 <myoshi878@gmail.com>
2022-02-19 18:48:44 +0900

    postモデルにimgカラムを連携させる

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true
+  mount_uploader :img, ImageUploader
 
   def liked_by?(user)
     likes.exists?(user_id: user.id)
