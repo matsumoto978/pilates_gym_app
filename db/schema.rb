ddeeae75957105a3da2fddd52f8b16591555ccb4
matsumoto978 <myoshi878@gmail.com>
2022-03-03 18:31:22 +0900

    wardカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_131404) do
+ActiveRecord::Schema.define(version: 2022_03_03_092845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 2022_02_24_131404) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "likes_count", default: 0
     t.string "img"
-    t.integer "ward", null: false
     t.text "URL"
     t.string "TEL"
     t.string "address", null: false
