d6d56e091847cf313584b479e3a47b1f807a5d80
matsumoto978 <myoshi878@gmail.com>
2022-03-03 18:18:31 +0900

    検索機能の編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,18 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: %i[edit update destroy]
+  before_action :set_post_q, { only: [:index, :show, :search] }
 
   PER_PAGE = 12
 
   def index
-    @q = Post.ransack(params[:q])
-    @posts = @q.result.page(params[:page]).per(PER_PAGE).includes(:user, :likes, :comments).order(created_at: :desc)
+    if params[:q].present?
+      @posts = @q.result.page(params[:page]).includes(:user, :likes).per(PER_PAGE)
+      @count = @posts.total_count
+    else
+      params[:q] = { sorts: "created_at desc" }
+      @posts = Post.order(:created_at).page(params[:page]).per(PER_PAGE)
+    end
   end
 
   def new
@@ -24,8 +30,7 @@ class PostsController < ApplicationController
     @comments = @post.comments.includes(:user, :post).order(created_at: :desc)
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @post.update!(post_params)
@@ -37,6 +42,10 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
+  def search
+    @results = @q.result
+  end
+
   private
 
   def post_params
@@ -45,5 +54,9 @@ class PostsController < ApplicationController
 
   def set_post
     @post = current_user.posts.find(params[:id])
+  end
+
+  def set_post_q
+    @q = Post.ransack(params[:q])
   end
 end
