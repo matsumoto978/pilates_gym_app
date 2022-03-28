2a27720044cd2295ed8d3fd3eca0392f564e237d
matsumoto978 <myoshi878@gmail.com>
2022-03-28 23:43:07 +0900

    enumsの日本語化

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module PilatesGymApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.available_locales = [:ja, :en]
     config.time_zone = "Asia/Tokyo"
     # Configuration for the application, engines, and railties goes here.
     #
